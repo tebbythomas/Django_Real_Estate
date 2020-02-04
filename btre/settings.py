ec0afac0ffabeacf923ffd14d463d4a54e645b59
Tebby Thomas <tebby.thomas@gmail.com>
2020-02-04 21:52:58 +0530

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = os.environ['EMAIL_USER']
 EMAIL_HOST_PASSWORD = os.environ['EMAIL_PASS']
 EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
