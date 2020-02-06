d098465ff2313fde61648a3abaade618177896ff
Tebby Thomas <tebby.thomas@gmail.com>
2020-02-06 22:13:08 +0530

    Editing settings.py

--- btre/settings.py
+++ btre/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'ttredb',
         'USER': 'postgres',
-        'PASSWORD': 'razacyazid',
+        'PASSWORD': os.environ['TOI_LOGIN_PASSWORD'],
         'HOST': 'localhost'
     }
 }
